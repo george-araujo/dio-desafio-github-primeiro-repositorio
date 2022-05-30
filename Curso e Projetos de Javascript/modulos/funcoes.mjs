5166ed13c49d2309a1558cc82550bb27bb099b98
George Luiz Santos Araujo <georgelsaraujo2@gmail.com>
2022-05-30 19:04:20 -0300

    Exercicio de modulo

---
+++ Curso e Projetos de Javascript/modulos/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostraHobby(nome, hobby){
+    return `O hobby de ${nome} é ${hobby}`
+}
+
+function mostraCidade(nome, cidade){
+    return `A cidade de ${nome} é ${cidade}`
+}
+
+export{
+    mostraIdade,
+    mostraHobby,
+    mostraCidade,
+}
---
+++ Curso e Projetos de Javascript/modulos/main.mjs
@@ -0,0 +1,10 @@
+import {mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+const nome = "George"
+const idade = "26"
+const hobby = "assistir videos"
+const cidade = "Campina Grande"
+
+console.log(mostraIdade(nome, idade));
+console.log(mostraHobby(nome, hobby));
+console.log(mostraCidade(nome, cidade));
